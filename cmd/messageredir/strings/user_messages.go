1573ebd25036195fc40bd1e1bc771059b5ecb5dc
Alexander Chernoskutov <alecherov@gmail.com>
2024-10-12 15:34:28 +0400

    Fix typo

--- cmd/messageredir/strings/user_messages.go
+++ cmd/messageredir/strings/user_messages.go
@@ -4,5 +4,5 @@ const (
 	BotMsgHelp  = "Hello! I am a message redir bot.\nSend /start command to generate new token or /end to leave the service.\nThis is an open-source project: https://github.com/AlexIII/messageredir"
 	MsgRedirFmt = "From: %s\nDate: %s\nSim: %s\n\n%s"
 	UserRemoved = "You were erased from the system. Goodbye!"
-	UserAdded   = "You are good to go!\nYour token: %s\n\nUse the following URL for to submit messages."
+	UserAdded   = "You are good to go!\nYour token: %s\n\nUse the following URL to submit messages."
 )
